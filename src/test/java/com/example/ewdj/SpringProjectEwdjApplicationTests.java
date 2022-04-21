fafbe799143d0a39f37f3f94a2100c17c1c33767
Laurens Arnauts <laurens.arnauts@student.hogent.be>
2022-04-21 10:02:50 +0200

    Initial commit

---
+++ src/test/java/com/example/ewdj/SpringProjectEwdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ewdj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectEwdjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
